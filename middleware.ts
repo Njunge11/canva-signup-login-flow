70ef8c173c7f69e3ce99cddf0799f9e9b3768b92
Njunge11 <njungedev@gmail.com>
2023-01-07 01:14:32 +0300

    add email login using magic link

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-  pages: { signIn: "/login" },
   callbacks: {
     authorized({ req, token }) {
       return !!token;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,37 +1,45 @@
 /** @format */
 
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 import AppleProvider from "next-auth/providers/apple";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import MicrosoftProvider from "next-auth/providers/azure-ad";
-
-/**
- * apple
- * google
- * facebook
- * microsoft
- * email
- * mobile
- */
+import EmailProvider from "next-auth/providers/email";
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     // AppleProvider({}),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
     // MicrosoftProvider({}),
   ],
-  callbacks: {
-    async jwt({ token }) {
-      return token;
-    },
+  pages: { signIn: "/login" },
+  session: {
+    strategy: "jwt",
+    maxAge: 30,
   },
 };
 
